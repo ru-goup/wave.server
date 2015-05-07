a8020a0e1bf8c094cad75aedc53f7761cc49ce94
Димон Дурак <dimon.durak@gmail.com>
2015-05-07 23:21:39 +0300

    Исправлено: wave.server.cmd

---
+++ wave.server.cmd
@@ -0,0 +1,6 @@
+node --stack-trace-limit=1000 --allow-natives-syntax wave.server.js
+
+@REM Uncomment this to disable automatic GC and call gs() manually
+@REM node --nouse-idle-notification --expose-gc wave.server.js
+
+pause
